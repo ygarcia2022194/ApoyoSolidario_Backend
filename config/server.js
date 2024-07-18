36c6083561585229dd97f35e704f7336b65c63a1
Gary Galindo <134737083+ggalindo2022147@users.noreply.github.com>
2024-07-17 22:41:35 -0600

    agregando las routes

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import publicacionesRoutes from '../publication/publication.routes.js'
 import { dbConnection } from './mongo.js';
 import {comprobarInformacion} from '../src/publication/publication.controller.js';
 
@@ -10,7 +11,8 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-
+        this.publicacionesPath = '/apoyoSolidario/v1'
+        
         this.middlewares();
         this.conectDB();
         this.routes();
@@ -30,7 +32,7 @@ class Server{
     }
 
     routes(){
-
+        this.app.use(this.publicacionesPath, publicacionesRoutes)
     }
 
     listen(){
